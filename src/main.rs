5c83ba3ee4be287a4b8ca7fbc17a283ed926179d
Luca Bracone <lucabr123@gmail.com>
2024-05-11 16:30:56 +0200

    mostly done with htmx tutorial

--- src/main.rs
+++ src/main.rs
@@ -2,22 +2,23 @@ use axum::{
     extract::{rejection::FormRejection, Path, Query, State},
     http::StatusCode,
     response::{Html, IntoResponse, Redirect, Result},
-    routing::{get, post},
+    routing::{delete, get, post},
     Form, Router,
 };
-use itertools::Itertools;
 use once_cell::sync::Lazy;
+use reqwest::Client;
 use serde::{Deserialize, Serialize};
 use serde_json::json;
 use std::{
     collections::HashMap,
-    sync::{self, Arc, Mutex},
+    sync::{Arc, Mutex},
 };
 use tera::{Context, Tera};
+use tracing::debug;
 
 type AppState = State<Arc<Mutex<Vec<Contact>>>>;
 
-#[derive(Clone, Serialize, Deserialize, Default)]
+#[derive(Debug, Clone, Serialize, Deserialize, Default)]
 struct Contact {
     first: String,
     last: String,
@@ -61,20 +62,22 @@ async fn contacts(
     State(contacts): AppState,
     Query(params): Query<HashMap<String, String>>,
 ) -> Result<impl IntoResponse, AppError> {
-    let q = params.get("q").cloned();
-    let contacts = contacts.lock().unwrap();
+    let q = params.get("q");
+    let mut contacts = contacts.lock().unwrap().clone();
+    if let Some(q) = q {
+        contacts.retain(|contact| contact.first.contains(q));
+    }
+    let page: usize = params
+        .get("page")
+        .and_then(|page| page.parse().ok())
+        .unwrap_or(0);
 
-    let contacts = match q {
-        Some(ref q) => contacts
-            .iter()
-            .filter(|contact| contact.first.contains(q))
-            .cloned()
-            .collect_vec(),
-        None => contacts.to_vec(),
-    };
+    contacts = contacts.chunks(1).nth(page).unwrap_or_default().to_vec();
+    debug!("{:?}", &contacts);
     let context = Context::from_serialize(json!({
         "query": q,
         "contacts": contacts,
+        "page": page
     }))?;
     Ok(Html(TEMPLATES.render("index.html", &context)?))
 }
@@ -83,21 +86,21 @@ async fn contacts_view(
     State(contacts): AppState,
     Path(id): Path<usize>,
 ) -> Result<impl IntoResponse, AppError> {
-    let contacts = contacts.lock().unwrap();
-    let Some(contact) = contacts.get(id) else {
-        return Ok((
+    if let Some(contact) = contacts.lock().unwrap().get(id) {
+        let context = Context::from_serialize(json!({
+            "contact": contact,
+            "id": id,
+        }))?;
+        Ok((
+            StatusCode::OK,
+            Html(TEMPLATES.render("show.html", &context)?),
+        ))
+    } else {
+        Ok((
             StatusCode::NOT_FOUND,
             Html(format!("Could not find contact with id {id}")),
-        ));
-    };
-    let context = Context::from_serialize(json!({
-        "contact": contact,
-        "id": id,
-    }))?;
-    Ok((
-        StatusCode::OK,
-        Html(TEMPLATES.render("show.html", &context)?),
-    ))
+        ))
+    }
 }
 
 async fn new_contact_form() -> Result<impl IntoResponse, AppError> {
@@ -111,21 +114,21 @@ async fn edit_contact_form(
     Path(id): Path<usize>,
     State(contacts): AppState,
 ) -> Result<impl IntoResponse, AppError> {
-    let contacts = contacts.lock().unwrap();
-    let Some(contact) = contacts.get(id) else {
-        return Ok((
+    if let Some(contact) = contacts.lock().unwrap().get(id) {
+        let context = Context::from_serialize(json!({
+            "contact": contact,
+            "id": id,
+        }))?;
+        Ok((
+            StatusCode::OK,
+            Html(TEMPLATES.render("edit.html", &context)?),
+        ))
+    } else {
+        Ok((
             StatusCode::NOT_FOUND,
             Html(format!("Could not find contact with id {id}")),
-        ));
-    };
-    let context = Context::from_serialize(json!({
-        "contact": contact,
-        "id": id,
-    }))?;
-    Ok((
-        StatusCode::OK,
-        Html(TEMPLATES.render("edit.html", &context)?),
-    ))
+        ))
+    }
 }
 
 async fn new_contact_request(
@@ -165,13 +168,51 @@ async fn delete_contact_request(
     State(contacts): AppState,
     Path(id): Path<usize>,
 ) -> impl IntoResponse {
-    let mut contacts = contacts.lock().unwrap();
-    contacts.swap_remove(id);
+    contacts.lock().unwrap().swap_remove(id);
     Redirect::to("/contacts")
 }
 
+#[derive(Serialize, Deserialize, Debug)]
+struct SpotifyToken {
+    access_token: String,
+    expires_in: u64,
+    token_type: String,
+}
+
 #[tokio::main]
-async fn main() {
+async fn main() -> anyhow::Result<()> {
+    // let client = Client::new();
+    // let res = client
+    //     .post("https://accounts.spotify.com/api/token")
+    //     .header("Content-Type", "aaplication/x-www-form-urlencoded")
+    //     .form(&[
+    //         ("grant_type", "client_credentials"),
+    //         ("client_id", "client_id"),
+    //         ("client_secret", "secret"),
+    //     ])
+    //     .send()
+    //     .await?;
+    // let token: SpotifyToken = res.json().await.unwrap();
+    //
+    // println!("{token:#?}");
+    //
+    // let result = client
+    //     .get("https://api.spotify.com/v1/search")
+    //     .query(&[("q", "classic"), ("type", "track")])
+    //     .header(
+    //         "Authorization",
+    //         format!("{} {}", token.token_type, token.access_token),
+    //     )
+    //     .send()
+    //     .await?
+    //     .json::<serde_json::Value>()
+    //     .await
+    //     .unwrap();
+    //
+    // println!("{result:#?}");
+    //
+    // Ok(())
+
     #[cfg(debug_assert)]
     TEMPLATES.full_reload();
 
@@ -187,17 +228,26 @@ async fn main() {
             phone: 123_123,
         },
     ]));
+
+    tracing_subscriber::fmt::fmt()
+        .pretty()
+        .with_max_level(tracing::Level::DEBUG)
+        .init();
+
     let app = Router::new()
         .route("/", get(index))
         .route("/contacts", get(contacts))
         .route("/contacts/:id", get(contacts_view))
         .route("/contacts/:id/edit", get(edit_contact_form))
         .route("/contacts/:id/edit", post(edit_contact_request))
-        .route("/contacts/:id/delete", post(delete_contact_request))
+        .route("/contacts/:id", delete(delete_contact_request))
         .route("/contacts/new", get(new_contact_form))
         .route("/contacts/new", post(new_contact_request))
-        .with_state(state);
+        .with_state(state)
+        .layer(tower_http::trace::TraceLayer::new_for_http());
 
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
-    axum::serve(listener, app).await.unwrap();
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await?;
+    axum::serve(listener, app).await?;
+
+    Ok(())
 }
